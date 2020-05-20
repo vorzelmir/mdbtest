31d1420380499e7eac14f4dcfcd2307c1bddebf4
vorzelmir <vorzelmir@gmail.com>
2020-05-20 14:20:26 +0300

    mapping between comboboxes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QSqlError>
 #include <QMessageBox>
 #include <QSqlRelationalDelegate>
+#include <QDataWidgetMapper>
 
 const auto PATTERN_TABLE = QStringLiteral ("pattern_system");
 const auto GROUP_TABLE = QStringLiteral ("measurement_group");
@@ -24,27 +25,32 @@ MainWindow::MainWindow(QWidget *parent)
     groupModel = new VGroupModel(this);
     measModel = new VMeasurementModel(this);
 
+    //init all the tables
     propertyModel->AddProperty();
     patternModel->InitPatternTable();
     groupModel->InitGroupTable();
     measModel->InitMEASTable();
 
+    //add general model to set the data
     relationModel = new QSqlRelationalTableModel(ui->databaseTable);
     relationModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
 
+    //in the view will be show measurement table
     relationModel->setTable(MEAS_TABLE);
+    //indexes of foreign keys
     int indexPattern = relationModel->fieldIndex("pattern");
     int indexGroup = relationModel->fieldIndex("group_name");
 
+    //create relation to measurement_group and pattern_system tables
     relationModel->setRelation(indexPattern, QSqlRelation(PATTERN_TABLE, "description", "description"));
     relationModel->setRelation(indexGroup, QSqlRelation(GROUP_TABLE, "name", "name"));
 
-    relationModel->setHeaderData(relationModel->fieldIndex("pattern"), Qt::Horizontal, QObject::tr("Pattern Name"));
+    relationModel->setHeaderData(indexPattern, Qt::Horizontal, QObject::tr("Pattern Name"));
     relationModel->setHeaderData(relationModel->fieldIndex("name"), Qt::Horizontal, QObject::tr("Name"));
     relationModel->setHeaderData(relationModel->fieldIndex("full_name"), Qt::Horizontal, QObject::tr("Full Name"));
     relationModel->setHeaderData(relationModel->fieldIndex("description"), Qt::Horizontal, QObject::tr("Description"));
     relationModel->setHeaderData(relationModel->fieldIndex("image"), Qt::Horizontal, QObject::tr("Image"));
-    relationModel->setHeaderData(relationModel->fieldIndex("group_name"), Qt::Horizontal, QObject::tr("Group Name"));
+    relationModel->setHeaderData(indexGroup, Qt::Horizontal, QObject::tr("Group Name"));
 
     if (!relationModel->select())
     {
@@ -54,10 +60,29 @@ MainWindow::MainWindow(QWidget *parent)
     }
 
     ui->databaseTable->setModel(relationModel);
-    ui->databaseTable->setColumnHidden(relationModel->fieldIndex("id"), true);
+
+    //create comboboxes of foreigner keys inside of ui->databaseTable
     ui->databaseTable->setItemDelegate(new QSqlRelationalDelegate());
+
+    ui->databaseTable->setColumnHidden(relationModel->fieldIndex("id"), true);
     ui->databaseTable->setSelectionMode(QAbstractItemView::SingleSelection);
     ui->databaseTable->setAutoScroll(true);
+
+    //two comboboxes with relation to the filds in the ui->databaseTable
+    ui->patternBox->setModel(relationModel->relationModel(indexPattern));
+    ui->patternBox->setModelColumn(relationModel->relationModel(indexPattern)->fieldIndex("description"));
+    ui->groupnameBox->setModel(relationModel->relationModel(indexGroup));
+    ui->groupnameBox->setModelColumn(relationModel->relationModel(indexGroup)->fieldIndex("name"));
+
+    //mapping between ui->databaseTable and ui->patternBox and ui->groupnameBox
+    QDataWidgetMapper *mapper = new QDataWidgetMapper(this);
+    mapper->setModel(relationModel);
+    mapper->setItemDelegate(new QSqlRelationalDelegate(this));
+    mapper->addMapping(ui->patternBox, indexPattern);
+    mapper->addMapping(ui->groupnameBox, indexGroup);
+
+    connect(ui->databaseTable->selectionModel(), &QItemSelectionModel::currentRowChanged,
+            mapper, &QDataWidgetMapper::setCurrentModelIndex);
 }
 
 
